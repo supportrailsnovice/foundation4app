b8793b1147afbc5708dc45a087e0788b44de3f38
rails-novice <ror.novice@gmail.com>
2013-07-10 09:59:56 +0530

    foundation 4 installed with rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foundation4::Application.config.secret_key_base = '8ddcc8ef9ca82e20fb25ac07d7c3f860a006351a81b7c5ac48a5525feb7247c9721b105707bbbdd9da4e8db5a27e3ebf0ce21d7090927b13641a0420a65d6f13'
